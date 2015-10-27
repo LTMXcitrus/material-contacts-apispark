a4f9b1bab79fa8f790ba9055060ec95a46500051
LTMXcitrus <mattcitron45140@gmail.com>
2015-10-27 15:37:47 +0100

    add test

---
+++ src/main/java/net/apispark/webapi/db/ContactPersistenceTest.java
@@ -0,0 +1,16 @@
+package net.apispark.webapi.db;
+
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class ContactPersistenceTest {
+
+	@Test
+	public void test() {
+		Assert.assertEquals(0, 0);
+		fail("Not yet implemented");
+	}
+
+}
